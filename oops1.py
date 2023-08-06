b71d13c4cbd9a9bce85e2d4bdb2dd32286339f86
vaibhav <vaibhavsachdeva2000@gmail.com>
2023-08-06 22:42:03 +0530

    method overloading

--- oops1.py
+++ oops1.py
@@ -1,5 +1,7 @@
 # operator overloading
 # we have different methods for different operators
+# method overloading
+# method overriding
 
 
 class Student:
@@ -7,16 +9,17 @@ class Student:
         self.m1 = m1
         self.m2 = m2
 
-    def __add__(self, other):
-        m1 = self.m1 + other.m1
-        m2 = self.m2 + other.m2
-        s3 = Student(m1, m2)
-        return s3
+    def sum(self, a=None, b=None, c=None):
+        s = 0
+        if a != None and b != None and c != None:
+            s = a + b + c
+        elif a != None and b != None:
+            s = a + b
+        else:
+            s = a
+        return s
 
 
-s1 = Student(58, 40)
-s2 = Student(56, 44)
+s1 = Student(59, 69)
 
-s3 = s1 + s2
-
-print(s3.m1)
+print(s1.sum(5, 9))

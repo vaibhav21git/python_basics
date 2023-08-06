01171af80fe8be711eadd482d65127ae5eb447d5
vaibhav <vaibhavsachdeva2000@gmail.com>
2023-08-06 18:35:16 +0530

    class inside a class

--- oops1.py
+++ oops1.py
@@ -1,38 +1,26 @@
-# get the value of variables  - accesors
-# modify the value of variables - mutators
-# @classmethod are decorators
-# static method is nothing to do with class variable and instance variable
-# why none is printing in staticmethod doubt
-
-
 class Student:
-    school = "vaibhav university"
+    def __init__(self, name, rollno):
+        self.name = name
+        self.rollno = rollno
+        self.lap = self.Laptop()
 
-    def __init__(self, m1, m2, m3):
-        self.m1 = m1
-        self.m2 = m2
-        self.m3 = m3
+    def show(self):
+        print(self.name, self.rollno)
+        self.lap.show()
 
-    def avg(self):
-        return (self.m1 + self.m2 + self.m3) / 3
+    # inner class
+    class Laptop:
+        def __init__(self):
+            self.brand = "HP"
+            self.cpu = "i5"
+            self.ram = 8
 
-    def get_m1(self):
-        return self.m1
-
-    def set_m1(self, value):
-        self.m1 = value
-
-    @classmethod
-    def getschool(cls):
-        return cls.school
-
-    @staticmethod
-    def info():
-        print("this is student class in abc module")
+        def show(self):
+            print(self.brand, self.cpu)
 
 
-s1 = Student(99, 76, 34)
-s2 = Student(90, 55, 77)
+s1 = Student("Navin", 2)
+s2 = Student("vaibhav", 3)
 
 
-print(Student.info())
+s1.show()
